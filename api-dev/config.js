406c8c4b695eed8f4796a5bd38bc5e625af2bcb1
JeongCheol Kim <jeongcheolkim@homet-dev-app-ricky.local>
2019-12-30 18:14:13 +0900

    update

--- api-dev/config.js
+++ api-dev/config.js
@@ -6,8 +6,9 @@ export const PW = '12qw34ER';
 export const DB_TABLE = 'poker';
 export const API_KEY = 'ironright';
 export const SERVER_KEY = 'ironrightGameServer';
+
 export const FACEBOOK_APP_TOKEN = "1114957222227239|df601c25d0c8bafc620310c4dc3e01bc";
 export const FACEBOOK_DEBUG_TOKEN_API =  "https://graph.facebook.com/debug_token";
-export const FACEBOOK_TOKEN_API =  "https://graph.facebook.com/token";
+export const FACEBOOK_TOKEN_API =  "https://graph.facebook.com/access_token";
 export const DEFAULT_BANK = 1000;
 export const DEFAULT_PROFILE_IMAGE = "";
